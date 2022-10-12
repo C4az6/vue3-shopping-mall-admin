a0d2d86c6a67af411be1b5dc4ea03d5d7938a30f
伤心的瘦子 <1762458611@qq.com>
2022-10-13 04:22:26 +0800

    登录页面开发

--- src/pages/about.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>about页面</div>
-</template>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import {
 } from 'vue-router';
 
 import Index from '~/pages/index.vue'
-import About from '~/pages/about.vue'
+import Login from '~/pages/login.vue'
 import NotFound from '~/pages/404.vue'
 
 const routes = [
@@ -13,8 +13,9 @@ const routes = [
     component: Index
   },
   {
-    path: '/about',
-    component: About
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
     // 404路由的正则匹配规则
