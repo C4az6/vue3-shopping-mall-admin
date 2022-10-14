9c61b4e8d3f7adb9cb0ee3b36ad70618294dab44
伤心的瘦子 <1762458611@qq.com>
2022-10-15 05:35:30 +0800

    统计面板组件引入骨架屏

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
     <el-row :gutter="20">
+      <template v-if="panels.length === 0">
+        <el-col :span="6" v-for="i in 4" :key="i">
+          <el-skeleton style="width: 100%" animated loading>
+            <template #template>
+              <el-card shadow="hover" class="border-0">
+                <template #header>
+                  <div class="flex justify-between">
+                    <el-skeleton-item
+                      variant="text"
+                      style="width: 50%"
+                    /><el-skeleton-item variant="text" style="width: 10%" />
+                  </div>
+                </template>
+
+                <span class="text-3xl font-bold text-gray-500">
+                  <el-skeleton-item variant="h3" style="width: 80%" />
+                </span>
+                <el-divider />
+
+                <div class="flex justify-between text-sm text-gray-500">
+                  <el-skeleton-item variant="text" style="width: 50%" />
+                  <el-skeleton-item variant="text" style="width: 10%" />
+                </div>
+              </el-card>
+            </template>
+          </el-skeleton>
+        </el-col>
+      </template>
+
       <el-col
         :span="6"
         :offset="0"
