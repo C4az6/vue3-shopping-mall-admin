222d1f241bdb8ba9e7d739af5ff84a714845f27e
伤心的瘦子 <1762458611@qq.com>
2022-10-16 06:07:34 +0800

    图标模块-基础布局实现

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,25 @@ body {
   background-color: #f4f4f4 !important;
   height: 3px !important;
 }
+
+/* 修改滚动条样式 */
+::-webkit-scrollbar {
+  width: 4px;
+  height: 6px;
+}
+
+::-webkit-scrollbar-corner {
+  display: block;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 8px;
+  background-color: rgba(0, 0, 0, 0.2);
+}
+::-webkit-scrollbar-thumb,
+::-webkit-scrollbar-track {
+  border-right-color: transparent;
+  border-left-color: transparent;
+  background-color: rgba(0, 0, 0, 0.1);
+}
 </style>
--- src/pages/image/list.vue
+++ src/pages/image/list.vue
@@ -1,23 +1,76 @@
 <template>
-  <div>图库列表</div>
+  <el-container class="bg-white rounded" :style="{ height: h + 'px' }">
+    <el-header class="image-header"> Header </el-header>
+    <el-container>
+      <el-aside width="220px" class="image-aside">
+        <div class="top">
+          <div v-for="item in 100" :key="item">
+            {{ item }}
+          </div>
+        </div>
+        <div class="bottom">分页区域</div>
+      </el-aside>
+      <el-main class="image-main">
+        <div class="top">
+          <div v-for="item in 100" :key="item">
+            {{ item }}
+          </div>
+        </div>
+        <div class="bottom">分页区域</div>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script setup>
-// ====== import ======
-import { ref, reactive, computed } from "vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
-
-// ====== composition api init ======
-const router = useRouter();
-const store = useStore();
-// ====== state ======
-
-// ====== method ======
-
-// ====== computed ======
-
-// ====== other ======
+// 让高度撑满页面
+const windowHeight = window.innerHeight || document.body.clientHeight;
+// 最后的40是padding
+const h = windowHeight - 64 - 44 - 40;
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.image-header {
+  border-bottom: 1px solid #eee;
+  @apply flex items-center;
+}
+
+.image-aside {
+  border-right: 1px solid #eee;
+  position: relative;
+  .top {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 50px;
+    overflow-y: auto;
+  }
+  .bottom {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    @apply flex justify-center items-center;
+  }
+}
+
+.image-main {
+  position: relative;
+  .top {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 50px;
+    overflow-y: auto;
+  }
+  .bottom {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    @apply flex justify-center items-center;
+  }
+}
+</style>
