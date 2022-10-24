ba9bb23f1a3fbdcaac9b95c765e1adb553bce53a
伤心的瘦子 <1762458611@qq.com>
2022-10-24 17:23:37 +0800

    商品模块-完成新增和修改功能

--- src/pages/goods/list.vue
+++ src/pages/goods/list.vue
@@ -73,7 +73,7 @@
           <el-table-column align="center" prop="address" label="操作">
             <template #default="scope">
               <div v-if="searchForm.tab !== 'delete'">
-                <el-button class="px-1" type="primary" text size="small">修改</el-button>
+                <el-button class="px-1" type="primary" text size="small" @click="handleEdit(scope.row)">修改</el-button>
                 <el-button class="px-1" type="primary" text size="small">商品规格</el-button>
                 <el-button class="px-1" type="primary" text size="small">设置轮播图</el-button>
                 <el-button class="px-1" type="primary" text size="small">商品详情</el-button>
@@ -97,27 +97,68 @@
 
     <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
       <el-form :model="form" ref="formRef" :rules="rules" label-width="auto">
-        <el-form-item label="用户名">
-          <el-input v-model="form.username"></el-input>
+        <el-form-item label="商品名称" prop="title">
+          <el-input v-model="form.title" placeholder="请输入商品名称,不能超过60个字符"></el-input>
         </el-form-item>
 
-        <el-form-item label="密码">
-          <el-input v-model="form.password"></el-input>
+        <el-form-item label="封面" prop="cover">
+          <ChooseImage v-model="form.cover" />
         </el-form-item>
 
-        <el-form-item label="头像">
-          <ChooseImage v-model="form.avatar" />
-        </el-form-item>
-
-        <el-form-item label="所属管理员">
-          <el-select v-model="form.role_id" placeholder="请选择所属管理员">
-            <el-option v-for="(item, index) in roles" :key="index" :label="item.name" :value="item.id" />
+        <el-form-item label="商品分类" prop="category_id">
+          <el-select v-model="form.category_id" placeholder="请选择所属分类">
+            <el-option v-for="(item, index) in categoryList" :key="index" :label="item.name" :value="item.id" />
           </el-select>
         </el-form-item>
 
-        <el-form-item label="状态">
-          <el-switch v-model="form.status" :active-value="1" :inactive-value="0">
-          </el-switch>
+        <el-form-item label="商品描述" prop="desc">
+          <el-input v-model="form.desc" placeholder="选填,商品卖点" type="textarea"></el-input>
+        </el-form-item>
+
+        <el-form-item label="单位" prop="unit">
+          <el-input v-model="form.unit" placeholder="请输入单位" style="width: 50%;"></el-input>
+        </el-form-item>
+
+        <el-form-item label="总库存" prop="stock">
+          <el-input v-model="form.stock" type="number" style="width: 40%;"></el-input>
+          <template #append>
+            件
+          </template>
+        </el-form-item>
+
+        <el-form-item label="库存预警" prop="min_stock">
+          <el-input v-model="form.min_stock" type="number" style="width: 40%;"></el-input>
+          <template #append>
+            件
+          </template>
+        </el-form-item>
+
+        <el-form-item label="最低销售价" prop="min_price">
+          <el-input v-model="form.min_price" type="number" style="width: 40%;"></el-input>
+          <template #append>
+            元
+          </template>
+        </el-form-item>
+
+        <el-form-item label="最低原价" prop="min_oprice">
+          <el-input v-model="form.min_oprice" type="number" style="width: 40%;"></el-input>
+          <template #append>
+            元
+          </template>
+        </el-form-item>
+
+        <el-form-item label="库存显示" prop="stock_display">
+          <el-radio-group v-model="form.stock_display">
+            <el-radio :label="0">显示</el-radio>
+            <el-radio :label="1">隐藏</el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="是否上架" prop="status">
+          <el-radio-group v-model="form.status">
+            <el-radio :label="0">仓库</el-radio>
+            <el-radio :label="1">上架</el-radio>
+          </el-radio-group>
         </el-form-item>
       </el-form>
 
@@ -185,11 +226,18 @@ const { formDrawerRef,
         avatar: []
       },
       form: {
-        username: "",
-        password: "",
-        role_id: null,
-        status: 1,
-        avatar: ""
+        title: null,  // 商品名称
+        category_id: null,    // 商品分类ID
+        cover: null,    // 商品封面
+        desc: null,    // 商品描述
+        unit: "件",   // 商品单位
+        stock: 100,   // 总库存
+        min_stock: 10,    // 库存预警
+        status: 1,  // 是否上架 0仓库 1上架
+        stock_display: 1,  // 库存显示 0隐藏 1显示
+        min_price: 0,   // 最低销售价
+        min_oprice: 0   // 最低原价
+
       },
       getData,
       update: updateGoods,
