a742dfb80dd84c34635db4878e0ae523e1401fbe
伤心的瘦子 <1762458611@qq.com>
2022-10-24 11:48:40 +0800

    使用$slots替换useSlots

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -12,7 +12,7 @@
         <div class="flex items-center justify-center">
           <el-button size="small" type="primary" @click="$emit('search')">搜索</el-button>
           <el-button size="small" @click="$emit('reset')">重置</el-button>
-          <el-button v-if="hasShowSearch" size="small" text type="warning" @click="showSearch = !showSearch">
+          <el-button v-if="!!$slots.show" size="small" text type="warning" @click="showSearch = !showSearch">
             {{showSearch ? '收起' : '展开'}}
             <el-icon>
               <ArrowUp v-if="showSearch"></ArrowUp>
@@ -38,7 +38,8 @@ defineEmits(["search", "reset"]);
 
 const slots = useSlots();
 
-const hasShowSearch = ref(!!slots.show);
+// 直接在模版中使用 $slots.xxx 来判断插槽是否存在就OK了。
+// const hasShowSearch = ref(!!slots.show);
 
 </script>
 
