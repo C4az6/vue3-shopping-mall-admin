e2fceacf7000589548d1cb634e31951cf1216330
伤心的瘦子 <1762458611@qq.com>
2022-10-27 23:13:16 +0800

    商品模块-富文本编辑器插入图片功能实现

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -59,7 +59,7 @@ const init = {
   plugins:
     'wordcount visualchars visualblocks template searchreplace save quickbars preview pagebreak nonbreaking media insertdatetime importcss image help fullscreen directionality codesample code charmap link code table lists advlist anchor autolink autoresize autosave',
   toolbar:
-    "formats undo redo fontsizeselect fontselect ltr rtl searchreplace media | outdent indent aligncenter alignleft alignright alignjustify lineheight underline quicklink h2 h3 blockquote numlist bullist table removeformat forecolor backcolor bold italic strikethrough hr link preview fullscreen help ",
+    "formats undo redo fontsizeselect fontselect ltr rtl searchreplace media imageUpload | outdent indent aligncenter alignleft alignright alignjustify lineheight underline quicklink h2 h3 blockquote numlist bullist table removeformat forecolor backcolor bold italic strikethrough hr link preview fullscreen help ",
   content_style: "p {margin: 5px 0; font-size: 14px}",
   fontsize_formats: "12px 14px 16px 18px 24px 36px 48px 56px 72px",
   font_formats: "微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方= PingFang SC, Microsoft YaHei, sans- serif; 宋体 = simsun, serif; 仿宋体 = FangSong, serif; 黑体 = SimHei, sans - serif; Arial = arial, helvetica, sans - serif;Arial Black = arial black, avant garde;Book Antiqua = book antiqua, palatino; ",
@@ -67,6 +67,18 @@ const init = {
   elementpath: false,
   resize: false, // 禁止改变大小
   statusbar: false, // 隐藏底部状态栏
+  setup: (editor) => {    // 当前富文本编辑器的对象
+    editor.ui.registry.addButton('imageUpload', {
+      tooltip: '插入图片',
+      icon: 'image',
+      onAction() {
+        // 往编辑区域插入内容,这里是插入图片
+        editor.insertContent(
+          `<img src="http://tangzhe123-com.oss-cn-shenzhen.aliyuncs.com/public/62af04afde812.jpg" alt="" style="width: 100%;" />`
+        );
+      }
+    })
+  }
 }
 tinymce.init; // 初始化
 const content = ref(props.modelValue)
